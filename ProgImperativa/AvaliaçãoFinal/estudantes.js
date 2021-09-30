4c06a9ea51c735ee1d92fccab75680f894ae8152
RodrigoGottschall <89365470+RodrigoGottschall@users.noreply.github.com>
2021-09-29 22:34:11 -0300

    Create estudantes.js

---
+++ ProgImperativa/AvaliaçãoFinal/estudantes.js
@@ -0,0 +1,11 @@
+const Aluno = require('./aluno')
+var estudantes = [
+    new Aluno("Rafael",[10,6,5],0),
+    new Aluno("Carla",[9,8,7],0),
+    new Aluno("Fabiana",[10,6,5],0),
+    new Aluno("Joao",[6,3,4],0),
+    new Aluno("Maria",[0,1,3],3),
+    new Aluno("Barbara",[7.7,7.7,7.7],3),
+    new Aluno("Justino",[7.7,7.7,7.7],5)
+]
+module.exports = estudantes;
